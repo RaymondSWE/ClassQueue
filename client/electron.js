f66867cd295038762c1db0f22dfc67f51e41282f
a21rammo <a21rammo@outlook.com>
2023-09-04 13:22:25 +0200

    fixed electron, not working on macos

--- client/electron.js
+++ client/electron.js
@@ -1,39 +1,45 @@
 const { app, BrowserWindow } = require('electron');
 const isDev = require('electron-is-dev');
-const path = require('path');
-
-let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 900,
-    height: 680,
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
-  const startURL = isDev
-    ? 'http://localhost:3000'
-    : `file://${path.join(__dirname, '../build/index.html')}`;
-
-  mainWindow.loadURL(startURL);
-
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
+  // and load the index.html of the app.
+  // win.loadFile("index.html");
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
+  // Open the DevTools.
+  if (isDev) {
+    win.webContents.openDevTools({ mode: 'detach' });
+  }
 }
 
-app.on('ready', createWindow);
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow);
 
-app.on('window-all-closed', function () {
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on('activate', function () {
-  if (mainWindow === null) {
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
