4cf878d2460ad15330ee93120612c5095aaf10e5
a21rammo <a21rammo@outlook.com>
2023-09-04 13:13:24 +0200

    spring boot as backend now

---
+++ server/Backend-queue/src/main/java/com/example/Backendqueue/BackendQueueApplication.java
@@ -0,0 +1,13 @@
+package com.example.Backendqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendQueueApplication.class, args);
+	}
+
+}
---
+++ server/Backend-queue/src/test/java/com/example/Backendqueue/BackendQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backendqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
