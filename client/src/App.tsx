d419d8c464bb3777374e8af992b12950f2c2cc24
a21rammo <a21rammo@outlook.com>
2023-09-04 15:27:37 +0200

    added design for client gui with only input and current users in queue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import { StudentClient } from './components/StudentClient';
 
 function App() {
   return (
     < >
-      <Navbar />
+      <Navbar/>
+      <StudentClient />
     </>
   );
 }
---
+++ client/src/components/StudentClient.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import './StudentClient.css'
+interface Student {
+  name: string;
+}
+
+export const StudentClient: React.FC = () => {
+  const [name, setName] = useState<string>('');
+  const [queue, setQueue] = useState<Student[]>([]);
+
+    //mock for design
+  useEffect(() => {
+    const initialQueue = [
+      { name: 'Raman' },
+      { name: 'Nibar' }
+    ];
+    setQueue(initialQueue);
+  }, []);
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleJoinQueue = () => {
+    setQueue([...queue, { name }]);
+    setName('');
+    
+  };
+
+  return (
+    <div className="student-client">
+      <div className="input-section">
+        <input 
+          type="text" 
+          placeholder="Enter your name" 
+          value={name} 
+          onChange={handleNameChange} 
+        />
+        <button onClick={handleJoinQueue}>Join Queue</button>
+      </div>
+      <div className="queue-section">
+        <h2>Students in Queue:</h2>
+        <ul>
+          {queue.map((student, index) => (
+            <li key={index}>{student.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
