f6e12ca24676b58f1a820d7124386116f9fe8c89
a21rammo <a21rammo@outlook.com>
2023-09-04 14:31:46 +0200

    added navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div >
-      
-    </div>
+    < >
+      <Navbar />
+    </>
   );
 }
 
