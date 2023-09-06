2802d7565fce66f286033e3c3616701a456da176
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-09-06 18:09:03 +0200

    some small change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,8 +8,8 @@ function App() {
   return (
     < >
       <Navbar role="STUDENT"/>
-      <Login />
-      {/* <StudentClient /> */}
+      {/* <Login /> */}
+      <StudentClient />
     </>
   );
 }
