b0e79e93595d7a47e45117b879fba071ec6a82a9
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-09-06 19:36:05 +0200

    cors fixed, able to send via client to server to api

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,15 @@
+
+`
+0server/src/main/resources/application.properties,1\6\169c626ff28ddea10a7a2bcec6242cd73b10aa8d
+i
+9server/src/main/java/com/example/server/models/Users.java,c\1\c13e9e1f0cfc2556b14424e597b8b36623764604
+w
+Gserver/src/main/java/com/example/server/controller/QueueController.java,b\8\b8e2b520c24a783dd8a242c1fa1c0312c0a443b1
+p
+@server/src/main/java/com/example/server/config/ZeroMqConfig.java,9\b\9bedf688147e4b55be761e8727cfd43ced8dad09
+>
+server/pom.xml,0\8\08a8b343eb3d041b4e874e74bf19e4275b4be110
+n
+>server/src/main/java/com/example/server/ServerApplication.java,d\3\d311f0676b77d973b9ba37338abb908c8a239576
+u
+Eserver/src/main/java/com/example/server/config/WebSecurityConfig.java,a\a\aad29bfe5ad21fa3a859b803f16a3c4577f4a626
--- server/src/main/java/com/example/server/config/WebSecurityConfig.java
+++ server/src/main/java/com/example/server/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+/*
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
@@ -29,3 +30,20 @@ public class WebSecurityConfig {
     }
 
 }
+
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable() // Disable CSRF
+                .authorizeRequests()
+                .requestMatchers("/**").permitAll(); // Allow all requests
+
+        return http.build();
+    }
+}
+
+
+
