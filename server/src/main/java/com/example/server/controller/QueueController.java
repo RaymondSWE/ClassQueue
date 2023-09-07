983f1b88ee4dd09894825e5642b028ea44c2744d
nibarsito <nibarsito2@gmail.com>
2023-09-07 20:22:28 +0200

    reverted changes to sockets made form an earlyer branch

--- server/src/main/java/com/example/server/controller/QueueController.java
+++ server/src/main/java/com/example/server/controller/QueueController.java
@@ -7,47 +7,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.zeromq.ZMQ;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 public class QueueController {
 
     @Autowired
-    private ZMQ.Socket zmqResponseSocket;
-
-    @Autowired
-    private ZMQ.Socket zmqPublisherSocket;
-
-    private List<Users> queue = new ArrayList<>();
+    private ZMQ.Socket zmqRequestSocket;
 
     @PostMapping("/joinQueue")
     public String joinQueue(@RequestBody Users user) {
         String message = String.format("{\"enterQueue\":true,\"name\":\"%s\",\"clientId\":\"someUniqueId\"}", user.getUsername());
-        zmqResponseSocket.send(message);
+        zmqRequestSocket.send(message);
 
-        String response = zmqResponseSocket.recvStr();
-
-        queue.add(user);
-
-        // Broadcast queue status
-        broadcastQueueStatus();
+        String response = zmqRequestSocket.recvStr();
 
         return response;
-    }
-
-    private void broadcastQueueStatus() {
-        String queueStatus = "[";
-
-        for (int i = 0; i < queue.size(); i++) {
-            queueStatus += String.format("{\"ticket\": %d, \"name\": \"%s\"}", i, queue.get(i).getUsername());
-            if (i < queue.size() - 1) {
-                queueStatus += ",";
-            }
-        }
-
-        queueStatus += "]";
-
-        zmqPublisherSocket.send("queue " + queueStatus);
     }
 }
