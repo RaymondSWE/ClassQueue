93b8c344df4deaa391e641b2958d2d6db01db861
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-09-07 16:27:38 +0200

    testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { StudentClient } from "./components/StudentClient/StudentClient";
 import { Login } from "./components/Login/Login";
-import { Toaster } from 'react-hot-toast';
+import { Toaster, toast } from 'react-hot-toast';
+const { ipcRenderer } = window.require('electron');
 
 function App() {
+  const [zmqMessages, setZmqMessages] = useState<string[]>([]);
+
+  useEffect(() => {
+    ipcRenderer.on('zmq-message', (_event: any, message: string) => {
+      setZmqMessages(prev => [...prev, message]);
+      // Notifying via toast
+      toast(`New ZMQ message: ${message}`);
+    });
+
+    return () => {
+      ipcRenderer.removeAllListeners('zmq-message');
+    };
+  }, []);
+
   return (
     <>
-      <Toaster />  
+      <Toaster />
       <Navbar role="STUDENT" />
       {/* <Login /> */}
       <StudentClient />
+      <div className="zmq-messages">
+        <h3>ZeroMQ Messages:</h3>
+        <ul>
+          {zmqMessages.map((message, index) => (
+            <li key={index}>{message}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
