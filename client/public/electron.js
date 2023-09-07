15bfcabdf7438e6f55788a8785e80b5c1d6e1b27
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-09-07 16:40:00 +0200

    clean up code, zeromessage componenet added for testing purposes

--- client/public/electron.js
+++ client/public/electron.js
@@ -3,104 +3,76 @@ const isDev = require('electron-is-dev');
 const path = require('path');
 const zmq = require('zeromq');
 
-function createMainMenuWindow() {
-    const win = new BrowserWindow({
+const windowsConfigurations = {
+    mainMenu: {
         title: "Main Menu",
         width: 1200,
         height: 800,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-        },
-    });
-
-    win.loadURL(
-        isDev
-            ? 'http://localhost:3000/mainmenu'
-            : `file://${path.join(__dirname, '../build/mainmenu.html')}`
-    );
-
-    if (isDev) win.webContents.openDevTools({ mode: 'detach' });
-
-        // Setup ZMQ subscriber socket
-        const subscriber = zmq.socket('sub');
-        subscriber.connect('tcp://ds.iit.his.se:5555');
-        subscriber.subscribe('');
-        subscriber.on('message', (message) => {
-            win?.webContents.send('zmq-message', message.toString());
-        });
-    
-        // Setup ZMQ request socket (this is simplified, you'd use it when you need it)
-        const requester = zmq.socket('req');
-        requester.connect('tcp://ds.iit.his.se:5556');
-        // Example usage: requester.send('some request');
-}
-
-function createStudentWindow() {
-    const win = new BrowserWindow({
+        urlPath: '/mainmenu',
+        zmqConfig: true
+    },
+    student: {
         title: "Students",
         width: 800,
         height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-        },
-    });
-
-    win.loadURL(
-        isDev
-            ? 'http://localhost:3000/students'
-            : `file://${path.join(__dirname, '../build/students.html')}`
-    );
-
-    if (isDev) win.webContents.openDevTools({ mode: 'detach' });
-}
-
-function createSupervisorWindow() {
-    const win = new BrowserWindow({
+        urlPath: '/students'
+    },
+    supervisor: {
         title: "Supervisor",
         width: 800,
         height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-        },
-    });
-
-    win.loadURL(
-        isDev
-            ? 'http://localhost:3000/supervisor'
-            : `file://${path.join(__dirname, '../build/supervisor.html')}`
-    );
-
-    if (isDev) win.webContents.openDevTools({ mode: 'detach' });
-}
-
-function createServerWindow() {
-    const win = new BrowserWindow({
+        urlPath: '/supervisor'
+    },
+    server: {
         title: "Server",
         width: 800,
         height: 600,
+        urlPath: '/server'
+    }
+};
+
+function createWindow(config) {
+    const win = new BrowserWindow({
+        title: config.title,
+        width: config.width,
+        height: config.height,
         webPreferences: {
             nodeIntegration: true,
-            contextIsolation: false,
-        },
+            contextIsolation: false
+        }
     });
 
     win.loadURL(
         isDev
-            ? 'http://localhost:3000/server'
-            : `file://${path.join(__dirname, '../build/server.html')}`
+            ? `http://localhost:3000${config.urlPath}`
+            : `file://${path.join(__dirname, `../build${config.urlPath}.html`)}`
     );
 
     if (isDev) win.webContents.openDevTools({ mode: 'detach' });
+
+    if (config.zmqConfig) {
+        setupZmqListeners(win);
+    }
+}
+
+function setupZmqListeners(win) {
+    const subscriber = zmq.socket('sub');
+    subscriber.connect('tcp://ds.iit.his.se:5555');
+    subscriber.subscribe('');
+    subscriber.on('message', message => {
+        win.webContents.send('zmq-message', message.toString());
+    });
+
+    const requester = zmq.socket('req');
+    requester.connect('tcp://ds.iit.his.se:5556');
 }
 
 app.whenReady().then(() => {
-    createMainMenuWindow();
-    // createStudentWindow();
-    // createSupervisorWindow();
-    // createServerWindow();
+    createWindow(windowsConfigurations.mainMenu);
+    // Uncomment below to start other windows up at startup
+    // createWindow(windowsConfigurations.student);
+    // createWindow(windowsConfigurations.supervisor);
+    // createWindow(windowsConfigurations.server);
 });
 
 app.on('window-all-closed', () => {
@@ -111,9 +83,8 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-        createMainMenuWindow();
-        createStudentWindow();
-        createSupervisorWindow();
-        createServerWindow();
+        Object.values(windowsConfigurations).forEach(config => {
+            createWindow(config);
+        });
     }
 });
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { StudentClient } from "./components/StudentClient/StudentClient";
 import { Login } from "./components/Login/Login";
-import { Toaster, toast } from 'react-hot-toast';
-import useZmqListener from './hooks/useZmqListener';
+import { Toaster } from 'react-hot-toast';
+import ZmqMessageList from './components/ZmqMessageList/ZmqMessageList';
 
 function App() {
-  const zmqMessages = useZmqListener();
-
-  useEffect(() => {
-      zmqMessages.forEach(message => {
-          toast(`New ZMQ message: ${message}`);
-      });
-  }, [zmqMessages]);
-
   return (
     <>
       <Toaster />
       <Navbar role="STUDENT" />
       {/* <Login /> */}
       <StudentClient />
-      <div className="zmq-messages">
-        <h3>ZeroMQ Messages:</h3>
-        <ul>
-          {zmqMessages.map((message, index) => (
-            <li key={index}>{message}</li>
-          ))}
-        </ul>
-      </div>
+      <ZmqMessageList />
     </>
   );
 }
