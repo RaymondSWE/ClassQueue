55d15969c647eccfcf44b5ffa7072308fd8cfaae
nibarsito <nibarsito2@gmail.com>
2023-09-08 13:59:52 +0200

    minor ajustments

--- python-client/client.py
+++ python-client/client.py
@@ -2,6 +2,21 @@ import zmq
 import tkinter as tk
 from tkinter import simpledialog, messagebox, Listbox
 import uuid
+context = zmq.Context()
+
+#  Socket to talk to server
+print("Connecting to hello world server...")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5556")
+
+#  Do 10 requests, waiting each time for a response
+for request in range(10):
+    print(f"Sending request {request} ...")
+    socket.send_string("Hello")
+
+    #  Get the reply.
+    message = socket.recv()
+    print(f"Received reply {request} [ {message} ]")
 
 # ZeroMQ setup
 context = zmq.Context()
