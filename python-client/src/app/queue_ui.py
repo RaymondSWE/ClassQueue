dc5963452c1fd206f3ce1d9ea9af8fafee2b571b
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-09-08 02:47:43 +0200

    design

--- python-client/src/app/queue_ui.py
+++ python-client/src/app/queue_ui.py
@@ -1,40 +1,52 @@
 import tkinter as tk
 from tkinter import ttk, Listbox
+from ttkthemes import ThemedTk
 
-
-class QueueUI(tk.Tk):
+class QueueUI(ThemedTk):
     def __init__(self):
         super().__init__()
 
+        self.set_theme("plastik")
         self.title("Queue Client")
         self.geometry("400x300")
 
-        self.style = ttk.Style()
-        self.style.configure("TButton", background="#00b09b")
+        # Create a frame for the content
+        content_frame = ttk.Frame(self)
+        content_frame.pack(fill=tk.BOTH, expand=True)
 
-        self.create_widgets()
+        # Create an outer frame with padding
+        outer_frame = ttk.Frame(content_frame, padding=20)
+        outer_frame.grid(row=0, column=0, sticky="nsew")
 
-    def create_widgets(self):
-        self.input_section = ttk.Frame(self, padding="20")
-        self.input_section.grid(row=0, column=0, sticky="ew")
+        # Input section
+        input_section = ttk.Frame(outer_frame, style="TFrame")
+        input_section.grid(row=0, column=0, sticky="ew", pady=10)
 
-        self.name_entry = ttk.Entry(self.input_section)
-        self.name_entry.grid(row=0, column=0, sticky="ew", padx=5)
+        ttk.Label(input_section, text="Name:", style="TLabel").grid(row=0, column=0, padx=5, pady=5)
+        self.name_entry = ttk.Entry(input_section, style="TEntry")
+        self.name_entry.grid(row=0, column=1, padx=5)
+        self.join_queue_button = ttk.Button(input_section, text="Join Queue", style="TButton")
+        self.join_queue_button.grid(row=0, column=2, padx=5)
 
-        self.join_queue_button = ttk.Button(self.input_section, text="Join Queue")
-        # Command will be set later from client.py
-        self.join_queue_button.grid(row=0, column=1, padx=5)
+        # Queue section
+        queue_section = ttk.Frame(outer_frame, style="TFrame")
+        queue_section.grid(row=1, column=0, sticky="nsew")
 
-        self.queue_section = ttk.Frame(self, padding="20")
-        self.queue_section.grid(row=1, column=0, sticky="ew")
+        ttk.Label(queue_section, text="Students in Queue", style="TLabel").grid(row=0, column=0, columnspan=2)
+        self.queue_listbox = Listbox(queue_section, height=10, bg="#555555", fg="white",
+                                     selectbackground="#00b09b", selectforeground="white", borderwidth=0,
+                                     highlightthickness=0)
+        self.queue_listbox.grid(row=1, column=0, columnspan=2, pady=10)
 
-        self.queue_label = ttk.Label(self.queue_section, text="Students in Queue")
-        self.queue_label.grid(row=0, column=0, columnspan=2)
-
-        self.queue_listbox = Listbox(self.queue_section, height=10)
-        self.queue_listbox.grid(row=1, column=0, columnspan=2)
+        outer_frame.columnconfigure(0, weight=1)
+        outer_frame.rowconfigure(0, weight=1)
+        outer_frame.rowconfigure(1, weight=1)
 
     def update_queue(self, students):
         self.queue_listbox.delete(0, tk.END)
         for student in students:
             self.queue_listbox.insert(tk.END, student['name'])
+
+if __name__ == "__main__":
+    app = QueueUI()
+    app.mainloop()
