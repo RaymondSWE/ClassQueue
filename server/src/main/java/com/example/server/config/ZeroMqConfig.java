9a80efd66c580006f583569f3ab1704e0c735dfd
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-09-09 00:03:28 +0200

    added exceptionhandler and logging to see if it works

--- server/src/main/java/com/example/server/config/ZeroMqConfig.java
+++ server/src/main/java/com/example/server/config/ZeroMqConfig.java
@@ -1,4 +1,8 @@
 package com.example.server.config;
+
+import com.example.server.error.SocketBindingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zeromq.ZMQ;
@@ -6,24 +10,36 @@ import org.zeromq.ZMQ;
 @Configuration
 public class ZeroMqConfig {
 
+    private static final Logger logger = LoggerFactory.getLogger(ZeroMqConfig.class);
 
     @Bean
     public ZMQ.Context zmqContext() {
         return ZMQ.context(1);
     }
 
-    // Publisher to broadcast messages to clients
     @Bean
     public ZMQ.Socket zmqPublisherSocket(ZMQ.Context context) {
         ZMQ.Socket publisher = context.socket(ZMQ.PUB);
-    publisher.bind("tcp://*:5500");
+        try {
+            publisher.bind("tcp://*:5500");
+            logger.info("Publisher socket bound successfully to tcp://*:5500");
+        } catch (Exception e) {
+            logger.error("Error binding the publisher socket", e);
+            throw new SocketBindingException("Error binding the publisher socket: " + e.getMessage());
+        }
         return publisher;
     }
-    // Res for responding to each client request
+
     @Bean
     public ZMQ.Socket zmqResponseSocket(ZMQ.Context context) {
         ZMQ.Socket responseSocket = context.socket(ZMQ.REP);
-        responseSocket.bind("tcp://*:5600");
+        try {
+            responseSocket.bind("tcp://*:5600");
+            logger.info("Response socket bound successfully to tcp://*:5600");
+        } catch (Exception e) {
+            logger.error("Error binding the response socket", e);
+            throw new SocketBindingException("Error binding the response socket: " + e.getMessage());
+        }
         return responseSocket;
     }
 }
