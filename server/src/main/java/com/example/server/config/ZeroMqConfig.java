78c868b8421451268dfac7eb3735d77a43b567df
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-09-08 23:58:08 +0200

    added comments

--- server/src/main/java/com/example/server/config/ZeroMqConfig.java
+++ server/src/main/java/com/example/server/config/ZeroMqConfig.java
@@ -6,18 +6,20 @@ import org.zeromq.ZMQ;
 @Configuration
 public class ZeroMqConfig {
 
+
     @Bean
     public ZMQ.Context zmqContext() {
         return ZMQ.context(1);
     }
 
+    // Publisher to broadcast messages to clients
     @Bean
     public ZMQ.Socket zmqPublisherSocket(ZMQ.Context context) {
         ZMQ.Socket publisher = context.socket(ZMQ.PUB);
     publisher.bind("tcp://*:5500");
         return publisher;
     }
-
+    // Res for responding to each client request
     @Bean
     public ZMQ.Socket zmqResponseSocket(ZMQ.Context context) {
         ZMQ.Socket responseSocket = context.socket(ZMQ.REP);
