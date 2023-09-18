d499fc8bc4a14d469493687bf12c60bd715444e8
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-09-18 14:25:12 +0200

    fetched tickets from our own server now

--- python-client/src/utils/queue_logic.py
+++ python-client/src/utils/queue_logic.py
@@ -25,14 +25,13 @@ class QueueLogic:
             "name": name,
             "clientId": self.client_id
         }
-        # Send request to ZMQHandler (API)
-        api_response = self.zmq_handler.send_request(data, self.zmq_handler.req_socket)
+
 
         # Send request to serverHandler (Local server)
         server_response = self.server_handler.send_request(data, self.server_handler.req_socket)
 
         # Handle None response
-        if not api_response:
+        if not server_response:
             messagebox.showerror("Error", "API response error.")
             return
 
@@ -40,7 +39,7 @@ class QueueLogic:
             messagebox.showerror("Error", "Server response error.")
             return
 
-        ticket = api_response.get('ticket', None)
+        ticket = server_response.get('ticket', None)
         if ticket:
             messagebox.showinfo("Info", f"Joined the queue with ticket number: {ticket}")
             self.ui.start_heartbeat() # Heartbeats should be sent after it has joined the queue (I think)
