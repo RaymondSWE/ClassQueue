2fc62e2c409beec6fdba0be8e577943a4963ae20
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-09-23 09:25:34 +0200

    identation fixed

--- server/src/main/java/com/example/server/service/QueueService.java
+++ server/src/main/java/com/example/server/service/QueueService.java
@@ -14,25 +14,23 @@ public class QueueService {
     private static final Logger logger = LoggerFactory.getLogger(QueueService.class);
 
     private final List<Student> queue = new ArrayList<>();
-private String name;
-private int ticket=-1;
+    private String name;
+    private int ticket = -1;
 
 
-public int getTicket() {
-                for(int i=0; i<queue.size(); i++)
-            {
-                if(queue.get(i).getName().equals(this.name))
-                {
-                this.ticket=i;
+    public int getTicket() {
+        for (int i = 0; i < queue.size(); i++) {
+            if (queue.get(i).getName().equals(this.name)) {
+                this.ticket = i;
                 break;
-                }
             }
+        }
 
-    return ticket;
-}
+        return ticket;
+    }
 
     public void manageStudent(String name, String clientId) {
-        this.name=name;
+        this.name = name;
         Student existingStudent = queue.stream()
                 .filter(s -> s.getName().equals(name))
                 .findFirst()
@@ -40,28 +38,29 @@ public int getTicket() {
         if (existingStudent == null) {
             List<String> clientIds = new ArrayList<>();
             clientIds.add(clientId);
-                Student newStudent = new Student(name, clientIds);
+            Student newStudent = new Student(name, clientIds);
             addStudent(newStudent);
             logger.info("New student joined: {}", name);
-        } else if(!existingStudent.getClientIds().contains(clientId)) {
+        } else if (!existingStudent.getClientIds().contains(clientId)) {
             existingStudent.getClientIds().add(clientId);
         }
     }
 
     private void addStudent(Student student) {
-        if(!queue.contains(student)) {
+        if (!queue.contains(student)) {
             queue.add(student);
         }
     }
+
     public void removeStudentByName(String name) {
         queue.removeIf(student -> student.getName().equals(name));
         logger.info("Student removed: " + name);
     }
-//remove the first student in queue
-public Student removeFirstStudent()
-{
-    return queue.remove(0);
-}
+
+    //remove the first student in queue
+    public Student removeFirstStudent() {
+        return queue.remove(0);
+    }
     // Handle inactvice users dont know how though, function should be below.
 
     public List<Student> getQueue() {
