0f7c75456173405b5d5545c77d83dc18ae3a4312
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-09-23 11:02:33 +0200

    can now connect as supervisor

--- python-client/src/utils/supervisor_logic.py
+++ python-client/src/utils/supervisor_logic.py
@@ -1,16 +1,25 @@
-from config.server_handler import ServerHandler
+import json
+import logging
 
+from config.server_handler import ServerHandler
+from error.connection_exceptions import EmptyResponseError
 
 class SupervisorLogic:
     def __init__(self, ui):
         self.ui = ui
         self.server_handler = ServerHandler()
+        logging.basicConfig(level=logging.INFO)
 
+    ## Kinda spagetti code in connect_as supervisor fix it in the future
     def connect_as_supervisor(self, supervisor_name):
-        # Logic to connect as a supervisor.
         message = {"type": "supervisor", "supervisorName": supervisor_name, "addSupervisor": True}
         try:
             response = self.server_handler.send_request(message, self.server_handler.req_socket)
-            print("Connect as supervisor response:", response)
+            jsonResponse = response if isinstance(response, dict) else json.loads(response)
+            logging.info("Connect as supervisor response: %s", jsonResponse)
+        except json.JSONDecodeError:
+            logging.error("Received non-JSON response from server: %s", response)
+        except EmptyResponseError as se:
+            logging.error("Specific error occurred while connecting as supervisor: %s", se)
         except Exception as e:
-            print(f"Error connecting as supervisor: {e}")
+            logging.error("Unexpected error occurred while connecting as supervisor: %s", e)
