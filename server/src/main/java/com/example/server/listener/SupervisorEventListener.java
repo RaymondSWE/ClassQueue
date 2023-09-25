f8321518e4326ffb5d16f2343631b958d9170a36
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-09-25 19:09:14 +0200

    broadcast queue when supervisorconnected

--- server/src/main/java/com/example/server/listener/SupervisorEventListener.java
+++ server/src/main/java/com/example/server/listener/SupervisorEventListener.java
@@ -1,6 +1,7 @@
 package com.example.server.listener;
 
 import com.example.server.event.NewSupervisorEvent;
+import com.example.server.service.StudentService;
 import com.example.server.worker.PublisherWorker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
@@ -10,14 +11,17 @@ import org.springframework.stereotype.Component;
 public class SupervisorEventListener {
 
     private final PublisherWorker publisherWorker;
+    private final StudentService studentService;
 
     @Autowired
-    public SupervisorEventListener(PublisherWorker publisherWorker) {
+    public SupervisorEventListener(PublisherWorker publisherWorker, StudentService studentService) {
         this.publisherWorker = publisherWorker;
+        this.studentService = studentService;
     }
 
     @EventListener
     public void handleNewSupervisorEvent(NewSupervisorEvent event) {
         publisherWorker.broadcastSupervisorsStatus();
+        publisherWorker.broadcastQueue(studentService.getQueue());
     }
 }
