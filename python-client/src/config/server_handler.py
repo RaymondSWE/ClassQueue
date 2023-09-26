fb4caea8511745529fd97eb0fab512f330c3d3a3
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-09-26 21:13:08 +0200

    added requirements.txt

--- python-client/src/config/server_handler.py
+++ python-client/src/config/server_handler.py
@@ -9,7 +9,7 @@ from error.connection_exceptions import (
 
 
 class ServerHandler:
-    CLIENT_NUMBER = 2
+    CLIENT_NUMBER = 1
     MAX_RETRIES = 3
     RETRY_INTERVAL = 5
 
@@ -18,43 +18,39 @@ class ServerHandler:
         self.SUB_SOCKET_ADDRESS = f"tcp://{host}:{sub_port}"
         self.context = zmq.Context()
 
-    def ping_server(self):
-        try:
-            self.req_socket.send_json({"type": "ping"})
-            self.req_socket.setsockopt(zmq.RCVTIMEO, 5000)
-            response_data = self.req_socket.recv()
-            response = json.loads(response_data)
-            return response.get("message") == "pong"
-        except zmq.Again:
-            logging.warning("Timeout while waiting for a response from server.")
-            return False
-        except zmq.ZMQError as e:
-            logging.error(f"ZMQ Error: {e}")
-            return False
-        except json.JSONDecodeError as e:
-            logging.error(f"JSON Decode Error: {e}")
-            return False
-
     def connect(self):
         retries = 0
         while retries < self.MAX_RETRIES:
             try:
                 self.req_socket = self.context.socket(zmq.REQ)
+                self.req_socket.setsockopt(zmq.RCVTIMEO, 5000)  # Set a timeout for receive operations
                 self.req_socket.connect(self.REQ_SOCKET_ADDRESS)
 
                 self.sub_socket = self.context.socket(zmq.SUB)
                 self.sub_socket.connect(self.SUB_SOCKET_ADDRESS)
                 self.sub_socket.setsockopt_string(zmq.SUBSCRIBE, "queue")
                 self.sub_socket.setsockopt_string(zmq.SUBSCRIBE, "supervisors")
-                if self.ping_server():
-                    return True
-            except zmq.ZMQError:
-                pass
-            retries += 1
-            logging.warning(f"Error connecting to server. Retrying {retries}/{self.MAX_RETRIES} in {self.RETRY_INTERVAL} seconds")
-            time.sleep(self.RETRY_INTERVAL)
+
+                # Testing connection by sending a ping or a simple message.
+                self.req_socket.send_string("queue")
+                # If server is offline, it will throw a zmq.Again exception.
+                self.req_socket.recv_string()
+                return True  # Connection successful
+
+            except zmq.Again:
+                logging.warning("Timeout while waiting for a response from server.")
+            except zmq.ZMQError as e:
+                logging.error(f"ZMQ Error: {e}")
+            except Exception as e:
+                logging.error(f"Unexpected Error: {e}")
+            finally:
+                retries += 1
+                logging.warning(
+                    f"Error connecting to server. Retrying {retries}/{self.MAX_RETRIES} in {self.RETRY_INTERVAL} seconds...")
+                time.sleep(self.RETRY_INTERVAL)
+
         logging.error("Connection failed after reaching the maximum number of retries.")
-        return False
+        return False  # Connection failed
 
     def subscribe(self, topic):
         self.sub_socket.setsockopt_string(zmq.SUBSCRIBE, topic)
