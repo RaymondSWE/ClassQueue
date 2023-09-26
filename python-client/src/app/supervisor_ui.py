37c1f83749d5405e30dcbb8a1f3afa0de7fa6518
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-09-26 15:44:47 +0200

    identation fix

--- python-client/src/app/supervisor_ui.py
+++ python-client/src/app/supervisor_ui.py
@@ -17,8 +17,7 @@ class SupervisorUI(ThemedTk):
         content_frame.pack(fill=tk.BOTH, expand=True)
 
         # Input section
-        ttk.Label(content_frame, text="Supervisor Name:", font=("Arial", 14)).grid(row=0, column=0, padx=10, pady=10,
-                                                                                   sticky="w")
+        ttk.Label(content_frame, text="Supervisor Name:", font=("Arial", 14)).grid(row=0, column=0, padx=10, pady=10, sticky="w")
         self.name_entry = ttk.Entry(content_frame, font=("Arial", 12), width=30)
         self.name_entry.grid(row=0, column=1, padx=10, pady=10, sticky="w")
 
@@ -35,10 +34,11 @@ class SupervisorUI(ThemedTk):
         self.available_button.grid(row=0, column=4, padx=10, pady=10, sticky="w")
 
         # Message entry
-        ttk.Label(content_frame, text="Message:", font=("Arial", 14)).grid(row=1, column=0, padx=10, pady=10,
-                                                                           sticky="w")
+        ttk.Label(content_frame, text="Message:", font=("Arial", 14)).grid(row=1, column=0, padx=10, pady=10, sticky="w")
         self.message_entry = ttk.Entry(content_frame, font=("Arial", 12), width=30)
         self.message_entry.grid(row=1, column=1, padx=10, pady=10, sticky="w")
+
+
 
         # Attend student button
         self.attend_student_button = ttk.Button(content_frame, text="Attend Student", command=self.attend_queue)
@@ -46,8 +46,7 @@ class SupervisorUI(ThemedTk):
 
         # Queue and supervisors Listboxes
         self.queue_listbox = self._create_listbox(content_frame, "Students in Queue", row=2, column=0, columnspan=2)
-        self.supervisor_listbox = self._create_listbox(content_frame, "Connected Supervisors", row=2, column=2,
-                                                       columnspan=3)
+        self.supervisor_listbox = self._create_listbox(content_frame, "Connected Supervisors", row=2, column=2, columnspan=3)
 
         # Status Label for showing connection information
         self.status_label = ttk.Label(content_frame, text="", font=("Poppins", 12), anchor="e")
@@ -56,10 +55,8 @@ class SupervisorUI(ThemedTk):
         self.logic = SupervisorLogic(self)
 
     def _create_listbox(self, parent, title, row, column, columnspan=1):
-        ttk.Label(parent, text=title, font=("Arial", 14, "bold")).grid(row=row, column=column, columnspan=columnspan,
-                                                                       pady=10)
-        listbox = Listbox(parent, height=15, width=40, bg="#f5f5f5", fg="black", selectbackground="#00b09b",
-                          selectforeground="white", borderwidth=1, highlightthickness=0, font=("Arial", 12))
+        ttk.Label(parent, text=title, font=("Arial", 14, "bold")).grid(row=row, column=column, columnspan=columnspan, pady=10)
+        listbox = Listbox(parent, height=15, width=40, bg="#f5f5f5", fg="black", selectbackground="#00b09b", selectforeground="white", borderwidth=1, highlightthickness=0, font=("Arial", 12))
         listbox.grid(row=row + 1, column=column, padx=10, pady=10, columnspan=columnspan, sticky="ew")
         return listbox
 
--- python-client/src/config/server_handler.py
+++ python-client/src/config/server_handler.py
@@ -1,9 +1,11 @@
 import json
 import time
 import zmq
+from error.connection_exceptions import (
+    ConnectionError, DeserializationError, EmptyResponseError,
+    InvalidResponseError, SendMessageError
+)
 
-from error.connection_exceptions import (ConnectionError, EmptyResponseError, ServerError, 
-                                         DeserializationError, SendMessageError, InvalidResponseError)
 
 class ServerHandler:
     CLIENT_NUMBER = 2
@@ -47,7 +49,7 @@ class ServerHandler:
         except zmq.Again:
             return None
         except json.JSONDecodeError:
-            raise DeserializationErstartror("Error decoding JSON from server.")
+            raise DeserializationError("Error decoding JSON from server.")
 
     def send_request(self, message, socket):
         try:
@@ -65,11 +67,9 @@ class ServerHandler:
 
     def send_startup_message(self):
         try:
-            self.req_socket.send_json({"type": "startup", "client_number": ServerHandler.client_number})
-            ServerHandler.client_number += 1
-            reply = self.req_socket.recv_string()
-            print("startup message:", reply)
-            if "error" in reply:
-                raise ServerError("invalid message");
+            self.req_socket.send_json({"type": "startup", "client_number": self.CLIENT_NUMBER})
+            self.CLIENT_NUMBER += 1
+            reply = self.req_socket.recv()
+            print("Startup message:", reply)
         except zmq.ZMQError:
             print("Error sending startup message to server.")
